34bfcabef338ec9c2dc3e0882acb85f61cfd0347
Manish-Rawat-1186 <manishrawat1186@gmail.com>
2022-06-10 01:56:56 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import './index.css';
 
 const App = () => {
   
@@ -15,8 +16,18 @@ const App = () => {
     }, [])
     
 
-    return (<div>
-    { time.toLocaleString() }
+    return (
+    <div className="box">
+        <div className="date">
+            <h5>{time.toDateString()}</h5>
+        </div>
+        <div className="item">
+            <h4 className="item1">{ time.getHours() > 12 ? time.getHours() -12 : time.getHours() }  :  { time.getMinutes() } {time.getHours > 12 ? 'PM' : 'AM'  } </h4>
+        </div>
+        <div className="item">
+            <h1  className="item3" >{ time.getSeconds() } seconds</h1>
+        </div>
+    
     </div>)
 };
 
